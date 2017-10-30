90d430d74a8889ed493c7ed08e837cf42ba71236
Dani-Ray <daniel_raykov@abv.bg>
2017-10-30 09:19:36 +0200

    blah

--- new_file.c
+++ new_file.c
@@ -1,13 +1,40 @@
-#include<sdio.h> 
-#include<> 
-int main(int argc, char **argv){ 
+#include<stdio.h> 
+#include<sys/types.h>
+#include<sys/stat.h> 
+#include<fcntl.h> 
+#include<unistd.h>  
 
-		int fd = open( argv[1], O_RDONLY ); 
+#define SIZE 100
+
+int main(int argc, char **argv){ 
+ 	char buffer[SIZE];
+	
+	int fd = open( argv[1], O_RDONLY ); 
+	ssize_t read_bytes, wresult; 
+	ssize_t written = 0; 	
 	if( fd < 0 ) {
-	perror( "oppen"); 
-	return 1; 
+		perror( "oppen"); 
+		return 1; 
 	}
 
+	read_bytes = read(fd, buffer, SIZE);
+	if(read_bytes < 0 ){
+		perror("read");
+		return 3; 
+	}
+	 	
+	wresult = write(STDOUT_FILENO, buffer, SIZE);
+	if(wresult < 0){
+	perror("write"); 
+	return 4; 
+	}
+	
+	int cresult = close(fd);
+	if(cresult < 0) {
+		perror("close");
+		return 2;	
+	}
+	
 	return 0;
 
 } 
