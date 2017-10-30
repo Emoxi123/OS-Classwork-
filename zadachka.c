57c7e1ec789318b42ecc5f46416057e829f3f2b3
Dani-Ray <daniel_raykov@abv.bg>
2017-10-30 08:35:29 +0200

    blah

--- zadachka.c
+++ zadachka.c
@@ -6,12 +6,15 @@ int main(){
 	const char *s = "Hello, emko :) \n";
 	size_t len = strlen( s );
 	ssize_t wresult;	
-		
-	wresult = write( STDOUT_FILENO, s, len); 
+	size_t written = 0; 
+
+	while(written != len){	 { 	
+	wresult = write( STDOUT_FILENO, s + written, len - written); 
 	if(wresult < 0) {
 	perror("ima greshka"); 
 	return 1; 
-	}
-
+		}
+	written += wresult; 
+	} 
 	return 0; 
 }
